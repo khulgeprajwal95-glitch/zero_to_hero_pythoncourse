ff5beca72a65fd6913ca14097afed01de230832f
Prajwal Khulge <khulgeprajwal95@gmail.com>
2026-09-25 07:29:01 +0530

    Add files via upload

---
+++ Find factorial of N.py
@@ -0,0 +1,6 @@
+
+sum=1
+for i in range(1,5):
+    sum=sum * i
+
+print(sum) 
---
+++ Print multiplication table.py
@@ -0,0 +1,4 @@
+a = int(input("enter number to get multiplication table:"))
+
+for x in range(1,11):
+    print(a,"x",x,"=",a*x)
